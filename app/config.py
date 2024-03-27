56264195227dedb49be9ccd7bf2ccec3252da82a
Fogzan <fogzan@yandex.ru>
2024-03-27 23:07:01 +0300

    add app

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 SECRET_KEY = 'e341e6698cb20dd889d040a9be7d5fc129cb06255f349bd6ea3f901afe8d61b4'
 
 # SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://std_2059_test:12345678@std-mysql.ist.mospolytech.ru/std_2059_test'
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:secret@mysql_db/myapp'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:password123@mysql_db/app'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = True
 
